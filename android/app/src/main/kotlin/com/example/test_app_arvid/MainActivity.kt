4e4ea939fd813c9476ed86e9f36d87bfe92b2dcd
arvidwedtstein <bassgamer03@gmail.com>
2022-09-26 15:56:08 +0200

    Initial cummit

---
+++ android/app/src/main/kotlin/com/example/test_app_arvid/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_app_arvid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,245 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Arvid Test App',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+        textTheme: GoogleFonts.latoTextTheme(),
+      ),
+      home: const RandomWords(),
+    );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final images = <String>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = <String>{};
+
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 10, color: Colors.black38),
+                  borderRadius: const BorderRadius.all(Radius.circular(8))
+                ),
+                margin: const EdgeInsets.all(4),
+                child: Image.asset(pair)
+              );
+              // return ListTile(
+              //   title: Text( // Replace with image
+              //     pair,
+              //     style: _biggerFont,
+              //   ),
+              // );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                  color: Colors.amber,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Liked Images'),
+            ),
+            body: ListView(children: divided),
+            // body: Container(
+            //   decoration: const BoxDecoration(
+            //     color: Colors.black26
+            //   ),
+            //   child: Column(
+            //     children: divided,   
+            //   ),
+            // )
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      appBar: AppBar(  
+        title: const Text('Test App'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Images',
+          ),
+        ],
+      ),
+      // body: ListView.builder(
+      //   padding: const EdgeInsets.all(16.0),
+      //   itemBuilder: (context, i) {
+      //     if (i.isOdd) return const Divider();
+
+      //     final index = i ~/ 2; /*3*/
+
+      //     if (index >= _suggestions.length) {
+      //       _suggestions.addAll(generateWordPairs().take(10));
+      //     }
+
+      //     final alreadySaved = _saved.contains(_suggestions[index]);
+  
+      //     return ListTile(
+      //       title: Text(
+      //         _suggestions[index].asPascalCase,
+      //         style: _biggerFont,
+      //       ),
+      //       subtitle: Text(
+      //         _suggestions[index].asCamelCase,
+      //         style: _biggerFont,
+      //       ),
+      //       trailing: Icon(
+      //         alreadySaved ? Icons.favorite : Icons.favorite_border,
+      //         color: alreadySaved ? Colors.red : null,
+      //         semanticLabel: alreadySaved ? 'Removed from saved' : 'Save',
+      //       ),
+      //       onTap: () {
+      //         setState(() {
+      //           if (alreadySaved) {
+      //             _saved.remove(_suggestions[index]);
+      //           } else {
+      //             _saved.add(_suggestions[index]);
+      //           }
+      //         });
+      //       },
+      //     );
+      //   },
+      // ),
+      body: _buildGrid(),
+    );
+  }
+  
+  Widget _buildImageColumn() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black26
+      ),
+      child: Column(
+        children: [
+          _buildImageRow(1),
+          _buildImageRow(3),
+        ],    
+      ),
+    );
+  }
+
+  Widget _buildImageRow(int imageIndex) => Row(
+    children: [
+      _buildDecoratedImage(imageIndex),
+      _buildDecoratedImage(imageIndex + 1),
+    ],
+  );
+
+  Widget _buildDecoratedImage(int imageIndex) {
+    images.add('images/pic$imageIndex.png');
+    final alreadySaved = _saved.contains(images[imageIndex]);
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: alreadySaved ? 10 : 0, color: Colors.black38),
+          borderRadius: const BorderRadius.all(Radius.circular(8))
+        ),
+        margin: const EdgeInsets.all(4),
+        child: GestureDetector(
+          child: Image.asset('images/pic$imageIndex.png'),
+          onTap: () {
+            
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(images[imageIndex]);
+              } else {
+                _saved.add(images[imageIndex]);
+              }
+            });
+          },
+        )
+      ),
+    );
+  }
+
+  Widget _buildGrid() => GridView.extent(
+    maxCrossAxisExtent: 150,
+    padding: const EdgeInsets.all(4),
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    children: _buildGridTileList(4),
+  );
+  List<Container> _buildGridTileList(int count) {
+    return List.generate(
+      count, (i) {
+        images.add('images/pic$i.png');
+        final alreadySaved = _saved.contains(images[i]);
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.black38),
+            borderRadius: const BorderRadius.all(Radius.circular(3))
+          ),
+          margin: const EdgeInsets.all(0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset('images/pic$i.png'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  GestureDetector(
+                    child: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border_outlined),
+                    onTap: () {
+                      
+                      setState(() {
+                        if (alreadySaved) {
+                          _saved.remove(images[i]);
+                        } else {
+                          _saved.add(images[i]);
+                        }
+                      });
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
